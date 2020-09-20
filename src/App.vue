a6df247866acb376f99a8a8286b40ec15424d77b
MahmudJion <43998247+MahmudJion@users.noreply.github.com>
2020-09-20 17:54:31 +0600

    init setup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <navbar></navbar>
+    <products></products>
+    <cart></cart>
+    <toast
+      :show="toast.show"
+      :text="toast.text"
+      @hide-toast="hideToast"
+    ></toast>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue";
+import Products from "./components/Products.vue";
+import Cart from "./components/Cart.vue";
+import Toast from "./components/Toast.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+    Products,
+    Cart,
+    Toast,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    toast() {
+      return this.$store.getters.toast;
+    },
+  },
+  methods: {
+    hideToast() {
+      this.$store.commit("hideToast");
+    },
+  },
+};
 </script>
 
 <style>
